91527427fdf17cd6f1eb6f83a0a6879523a23d2d
phenrique7 <ph@t10.digital>
2022-08-26 15:37:25 -0300

    chore: update `NotFoundPage` stories

--- src/pages/not-found/NotFoundPage.stories.tsx
+++ src/pages/not-found/NotFoundPage.stories.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
-import type { StoryArgs } from '../../types/storybook';
-import { NotFoundPage } from './NotFoundPage';
+import type { StoryArgs } from '@/types/storybook';
+import { Layout } from '@/components/layout/Layout';
+import { NotFoundPage } from '@/pages/not-found/NotFoundPage';
 
 type TStory = StoryArgs<typeof NotFoundPage>;
 
 export default {
-  title: 'pages/NotFoundPage',
+  title: 'pages/Not Found Page',
   component: NotFoundPage,
+  args: {
+    routeEntries: ['/wrong-url'],
+  },
+  decorators: [
+    (Story) => (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="*" element={<Story />} />
+        </Route>
+      </Routes>
+    ),
+  ],
 } as ComponentMeta<TStory>;
 
 const Template: ComponentStory<TStory> = () => <NotFoundPage />;
