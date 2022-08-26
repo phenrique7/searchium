0697e64c978dc73dd12fdfc8533fe97450f30f12
phenrique7 <ph@t10.digital>
2022-08-26 15:36:29 -0300

    chore: show bg only in home page

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import FullScreen from 'react-div-100vh';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import {
   Box,
   Link,
@@ -15,30 +15,36 @@ import { LogoIcon } from '@/icons/LogoIcon';
 import { GitHubIcon } from '@/icons/GitHubIcon';
 
 export function Layout() {
+  const location = useLocation();
+
+  const route = location.pathname;
+
   return (
     <FullScreen>
-      <Box
-        w="full"
-        h="full"
-        position="absolute"
-        sx={{ maskImage: 'linear-gradient(to bottom, transparent, black)' }}
-        _before={{
-          w: 'full',
-          h: 'full',
-          content: '""',
-          position: 'absolute',
-          background: `rgb(248 250 252/1) url('/images/hero-light.jpg') no-repeat bottom`,
-        }}
-        _after={{
-          w: 'full',
-          h: 'full',
-          content: '""',
-          position: 'absolute',
-          backgroundSize: '50px 50px',
-          backgroundPosition: '0 0, 25px 25px',
-          backgroundImage: `radial-gradient(#ced4da 1px, transparent 0), radial-gradient(#ced4da 1px, transparent 0)`,
-        }}
-      />
+      {route === '/' ? (
+        <Box
+          w="full"
+          h="full"
+          position="absolute"
+          sx={{ maskImage: 'linear-gradient(to bottom, transparent, black)' }}
+          _before={{
+            w: 'full',
+            h: 'full',
+            content: '""',
+            position: 'absolute',
+            background: `rgb(248 250 252/1) url('/images/hero-light.jpg') no-repeat bottom`,
+          }}
+          _after={{
+            w: 'full',
+            h: 'full',
+            content: '""',
+            position: 'absolute',
+            backgroundSize: '50px 50px',
+            backgroundPosition: '0 0, 25px 25px',
+            backgroundImage: `radial-gradient(#ced4da 1px, transparent 0), radial-gradient(#ced4da 1px, transparent 0)`,
+          }}
+        />
+      ) : null}
       <Box as="main" zIndex={1} position="relative">
         <Flex
           as="header"
