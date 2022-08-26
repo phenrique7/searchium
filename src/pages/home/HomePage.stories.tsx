200891671706958b5f47612128753c4bdfc4902b
phenrique7 <ph@t10.digital>
2022-08-26 15:01:32 -0300

    chore: update stories for `<HomePage/>`

--- src/pages/home/HomePage.stories.tsx
+++ src/pages/home/HomePage.stories.tsx
@@ -1,12 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { HomePage } from '@/pages/home/HomePage';
 import type { StoryArgs } from '@/types/storybook';
+import { Layout } from '@/components/layout/Layout';
 
 type TStory = StoryArgs<typeof HomePage>;
 
 export default {
   title: 'pages/HomePage',
   component: HomePage,
+  decorators: [
+    (Story) => (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Story />} />
+        </Route>
+      </Routes>
+    ),
+  ],
 } as ComponentMeta<TStory>;
 
 const Template: ComponentStory<TStory> = () => <HomePage />;
